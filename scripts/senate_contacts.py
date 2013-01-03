3716a53a63732dfecb209fc79f5243bbd225b6b8
Eric Mill <konklone@gmail.com>
2013-01-03 18:57:10 -0500

    Updating Senators for 113th congress, including finding new bioguide IDs. Also ignoring pickle files.

--- scripts/senate_contacts.py
+++ scripts/senate_contacts.py
@@ -20,37 +20,53 @@ y = load_data("legislators-current.yaml")
 # Map bioguide IDs to dicts. Reference the same dicts
 # in y so we are updating y when we update biogiude.
 bioguide = { }
+by_name = { }
 for m in y:
-	bioguide[m["id"]["bioguide"]] = m
+	if m["id"].has_key("bioguide"):
+		bioguide[m["id"]["bioguide"]] = m
+	party = m["terms"][-1]["party"][0]
+	state = m["terms"][-1]["state"]
+	last_name = m["name"]["last"]
+	member_full = "%s (%s-%s)" % (last_name, party, state)
+	print member_full
+	by_name[member_full] = m
 
 url = "http://www.senate.gov/general/contact_information/senators_cfm.xml"
 body = download(url, "legislators/senate.xml", force)
 dom = lxml.etree.parse(StringIO.StringIO(body))
 for node in dom.getroot():
 	bioguide_id = str(node.xpath("string(bioguide_id)")).strip()
-	print "[%s] Processing Senator..." % bioguide_id
+	member_full = node.xpath("string(member_full)")
+
+	print "[%s] Processing Senator %s..." % (bioguide_id, member_full)
 	
-	if not bioguide_id in bioguide:
-		print "Missing member", bioguide_id
-		continue
-		
+	# find member record in our YAML, either by bioguide_id or member_full
+	if bioguide.has_key(bioguide_id):
+		member = bioguide[bioguide_id]
+	else:
+		if by_name.has_key(member_full):
+			member = by_name[member_full]
+		else:
+			print "Missing member", bioguide_id, member_full
+			exit(0)
+
 	try:
-		term = bioguide[bioguide_id]["terms"][-1]
+		term = member["terms"][-1]
 	except IndexError:
-		print "Member has no terms", bioguide_id
+		print "Member has no terms", bioguide_id, member_full
 		continue
 		
 	if today < parse_date(term["start"]) or today > parse_date(term["end"]):
-		print "Member's last listed term is not current", bioguide_id, term["start"]
+		print "Member's last listed term is not current", bioguide_id, member_full, term["start"]
 		continue
 		
 	if term["type"] != "sen":
-		print "Member's last listed term is not a Senate term", bioguide_id
+		print "Member's last listed term is not a Senate term", bioguide_id, member_full
 		continue
 		
 		
 	if term["state"] != str(node.xpath("string(state)")):
-		print "Member's last listed term has the wrong state", bioguide_id
+		print "Member's last listed term has the wrong state", bioguide_id, member_full
 		continue
 		
 	if "district" in term: del term["district"]
@@ -60,16 +76,20 @@ for node in dom.getroot():
 	if ", " in full_name: full_name, suffix = full_name.split(", ")
 	full_name += " " + unicode(node.xpath("string(last_name)"))
 	if suffix: full_name += ", " + suffix
-	bioguide[bioguide_id]["name"]["official_full"] = full_name
+	member["name"]["official_full"] = full_name
+
+	member["id"]["bioguide"] = bioguide_id
 
 	term["class"] = { "Class I": 1, "Class II": 2, "Class III": 3}[ node.xpath("string(class)") ]
 	term["party"] = { "D": "Democrat", "R": "Republican", "I": "Independent", "ID": "Independent"}[ node.xpath("string(party)") ]
 	
-	url = str(node.xpath("string(website)")).strip().replace("http://www.", "http://")
+	url = str(node.xpath("string(website)")).strip()
 	term["url"] = url
 	term["address"] = str(node.xpath("string(address)")).strip()
 	term["office"] = string.capwords(term["address"].split(" WASHINGTON ")[0])
 	
-	# TODO there is also an "email" field with a URL to a contact form (is it always a URL?)
+	contact_form = str(node.xpath("string(email)")).strip()
+	if contact_form: # can be blank
+		term["contact_form"] = contact_form
 
 save_data(y, "legislators-current.yaml")
