88fa8240648656d0144ae984547d131e7f191016
hugovk <hugovk@users.noreply.github.com>
2014-02-12 09:04:39 +0200

    Add Bioguide resolution using legislators YAML
    If the Bioguide ID isn't found in the member page, resolve it from the values in the YAML file.
    Resolves in this sort of order:
    last, first
    last, first middle
    official name
    last, nickname
    last, first middle-initial.
    last followed by , first
    ballotopedia name
    wikipedia name
    last, firstname
    last, firstnam
    last, firstn
    ...
    last, fi
    last, f
    
    Didn't resolve Bioguide IDs for four people.
    
    Bradley, Byrne 113/RP/Bradley -> because GPO has bad data. He's really "Byrne, Bradley".
    
    GPO data should be fixed (how to report?), but should we add a final resolution case for switched names?
    
    These three aren't in the YAML:
    Chiesa, Jeff 113/SR/Chiesa
    Radel, Trey 113/RP/Radel
    Young, C. W. Bill 113/RP/Young
    
    Chiesa left in 2013, Radel left in 2014, Young died in 2013.
    
    TODO: Add caching of downloaded pages, rate limiting.
    See https://github.com/unitedstates/congress-legislators/issues/160#issuecomment-34833782

--- scripts/gpo_member_photos.py
+++ scripts/gpo_member_photos.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*- 
 """
-Scrape http://www.memberguide.gpoaccess.gov and save members' photos named after their Bio IDs.
+Scrape http://www.memberguide.gpoaccess.gov and 
+save members' photos named after their Bioguide IDs.
 """
 import argparse
 from BeautifulSoup import BeautifulSoup # pip install BeautifulSoup
@@ -8,8 +10,10 @@ import mechanize # pip install mechanize
 import os
 import re
 import urlparse
+import yaml # pip install pyyaml
 
-# TODO could download http://unitedstates.sunlightfoundation.com/legislators/legislators.csv directly when needed
+# TODO: Cache downloaded member pages (see utils.download method)
+# TODO: Could download YAML directly when needed
 
 def get_front_page(br, congress_number):
     url = r'http://www.memberguide.gpoaccess.gov/GetMembersSearch.aspx'
@@ -22,11 +26,11 @@ def get_front_page(br, congress_number):
     br.set_handle_refresh(False)  # can sometimes hang without this
     br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 
-    print "Open..."
+    print "Open front page..."
     response = br.open(url).read()
 
     # print response.read()      # the text of the page
-    print 'href="' + congress_number in response
+    # print 'href="' + congress_number in response
     # print response
 
     ##############################
@@ -37,17 +41,6 @@ def get_front_page(br, congress_number):
     form = br.form
     form.set_all_readonly(False)    # allow everything to be written to
 
-    for control in br.form.controls:
-        if control.name == "ctl00_ContentPlaceHolder1_Memberstelerikrid_ctl00_ctl02_ctl00_GoToPageTextBox_ClientState":
-            print "ctl00_ContentPlaceHolder1_Memberstelerikrid_ctl00_ctl02_ctl00_GoToPageTextBox_ClientState"
-            print control
-            print "type=", control.type
-            print "name=", control.name
-            print "value=", br[control.name]
-        # print control
-        # print "type=%s, name=%s value=%s" % (control.type, control.name, br[control.name])    
-    # end
-    
     # The only submit button is "Clear Search" and we don't want to do that!
     for control in br.form.controls:
         if control.name == "ctl00$ContentPlaceHolder1$btnClear":
@@ -55,25 +48,20 @@ def get_front_page(br, congress_number):
             break
 
     # The search button is hooked up to a Javascript __doPostBack() function that sets __EVENTTARGET
-    # control = form.find_control("__EVENTTARGET")
-    print br['__EVENTTARGET']
     br['__EVENTTARGET']='ctl00$ContentPlaceHolder1$btnSearch'
-    print br['__EVENTTARGET']
 
     # Set the congress session number
-    print br['ctl00$ContentPlaceHolder1$ddlCongressSession']
     br['ctl00$ContentPlaceHolder1$ddlCongressSession']=[congress_number] # Use a list for select controls with multiple values
-    print br['ctl00$ContentPlaceHolder1$ddlCongressSession']
 
-    print "Submit..."
+    print "Submit congress session number..."
     response = br.submit().read()
 
-    print 'href="' + congress_number in response
+    # print 'href="' + congress_number in response
     br.select_form(nr=0)
     form = br.form
-    print br['ctl00$ContentPlaceHolder1$ddlCongressSession']
+    # print br['ctl00$ContentPlaceHolder1$ddlCongressSession']
 
-    # TODO could change members-per-page so we don't need to keep clicking next
+    # TODO: Could change members-per-page so we don't need to keep clicking next
     
     #############################
     # Choose next page until done
@@ -88,9 +76,12 @@ def get_front_page(br, congress_number):
         # Harvest links
         for link in br.links():
             if congress_number + "/" in link.url:
-                print link.text, link.url
-                links.append(link)
-        print "Page:", br['ctl00$ContentPlaceHolder1$Memberstelerikrid$ctl00$ctl03$ctl01$GoToPageTextBox']
+                if "/RP/" in link.url or "/SR/" in link.url:
+                    # Include only representatives and senators.
+                    # Exclude capitol, house and senate officials ("CO", "HO", "SO"), 
+                    # a president ("PR") and a vice-president ("VP") (8 in 113rd)
+                    print link.text, link.url
+                    links.append(link)
         print "Links:", len(links)
 
         # return links # TEMP!!! TODO!!! for testing
@@ -107,23 +98,16 @@ def get_front_page(br, congress_number):
                 break
 
         # The search button is hooked up to a Javascript __doPostBack() function that sets __EVENTTARGET
-        # control = form.find_control("__EVENTTARGET")
-        print br['__EVENTTARGET']
         br['__EVENTTARGET']='ctl00$ContentPlaceHolder1$Memberstelerikrid$ctl00$ctl02$ctl00$ctl28'
-        print br['__EVENTTARGET']
 
-        print "Submit..."
+        print "Submit next page..."
         response = br.submit().read()
 
-        print "href=\"112" in response
         print 'href="' + congress_number in response
         br.select_form(nr=0)
         form = br.form
         print br['ctl00$ContentPlaceHolder1$ddlCongressSession']
 
-        print br['ctl00$ContentPlaceHolder1$Memberstelerikrid$ctl00$ctl02$ctl00$ChangePageSizeTextBox']
-        print br['ctl00$ContentPlaceHolder1$Memberstelerikrid$ctl00$ctl03$ctl01$ChangePageSizeTextBox']
-        
         last_page = this_page
         this_page = br['ctl00$ContentPlaceHolder1$Memberstelerikrid$ctl00$ctl03$ctl01$GoToPageTextBox']
 
@@ -132,17 +116,99 @@ def get_front_page(br, congress_number):
     ###########################################
     return links
 
+def load_yaml(filename):
+    f = open(filename)
+    data = yaml.safe_load(f)
+    f.close()
+    return data
+
+def remove_from_yaml(data, bioguide_id):
+    data[:] = [d for d in data if d['id']['bioguide'] != bioguide_id]
+    return data
     
+def get_value(item, key1, key2):
+    value = None
+    if key2 in item[key1].keys():
+        value = item[key1][key2]
+    return value
+
+def resolve(data, text):
+    if isinstance(text, str):
+        text = text.decode('utf-8')
+
+    for item in data:
+        bioguide = item['id']['bioguide']
+        last = item['name']['last']
+        first = item['name']['first']
+        middle = get_value(item, 'name', 'middle')
+        nickname = get_value(item, 'name', 'nickname')
+        official = get_value(item, 'name', 'official_full')
+        text_reversed = reverse_names(text)
+        ballotpedia = get_value(item, 'id', 'ballotpedia')
+        wikipedia = get_value(item, 'id', 'wikipedia')
+
+        if text == last + ", " + first:
+            return bioguide
+        elif middle and text == last + ", " + first + " " + middle:
+            return bioguide
+        elif official and text_reversed == official:
+            return bioguide
+        elif nickname and text == last + ", " + nickname:
+            return bioguide
+        elif middle and text == last + ", " + first + " " + middle[0] + ".":
+            return bioguide
+        elif text.startswith(last) and ", " + first in text:
+            return bioguide
+        elif ballotpedia and ballotpedia == text_reversed:
+            return bioguide
+        elif wikipedia and wikipedia == text_reversed:
+            return bioguide
+
+        # Check all of first name, then all letters but last, ..., then first letter
+        for i in reversed(range(len(first))):
+            if text.startswith(last) and ", " + first[:i+1] in text:
+                return bioguide
+        
+        # TODO? Bradley, Byrne -> Byrne, Bradley (bad GPO data)
+
+    return None
+
+def reverse_names(text):
+    # Given names like "Hagan, Kay R.", reverse them to "Kay R. Hagan"
+    return ' '.join(text.split(',')[::-1]).strip(" ")
+
+def bioguide_id_from_url(url):
+    bioguide_id = urlparse.parse_qs(urlparse.urlparse(url).query)['index'][0].strip("/")
+    bioguide_id = bioguide_id.capitalize()
+    return bioguide_id
+
+def bioguide_id_valid(bioguide_id):
+    if not bioguide_id:
+        return False
+
+    # A letter then six digits
+    # For example C001061
+    
+    # TODO: Is this specification correct?
+    # Assume capital letter because ID finder will have uppercased it
+    if re.match(r'[A-Z][0-9][0-9][0-9][0-9][0-9]', bioguide_id):
+        return True
+    
+    return False
+
+
 def download_photos(br, member_links, outdir):
-    print "Found a total of", len(member_links), "links"
+    print "Found a total of", len(member_links), "member links"
     if not os.path.exists(outdir):
         os.makedirs(outdir)
 
     todo_resolve = []
+    legislators = load_yaml(args.yaml)
+
     for i, member_link in enumerate(member_links):
         print "---"
         print "Processing member", i+1, "of", len(member_links), ":", member_link.text
-        bio_id = None
+        bioguide_id = None
         
         response = br.follow_link(member_link)
         print response.geturl()
@@ -151,20 +217,34 @@ def download_photos(br, member_links, outdir):
         for link in br.links():
             if "bioguide.congress.gov" in link.url:
                 # print link.text, link.url
-                bio_id = urlparse.parse_qs(urlparse.urlparse(link.url).query)['index'][0].strip("/")
+                bioguide_id = bioguide_id_from_url(link.url)
+
+                # Validate Bioguide ID
+                # One member's link didn't contain the ID: http://young.house.gov
+                if not bioguide_id_valid(bioguide_id):
+                    bioguide_id = None
+
                 break
 
-        # if not bio_id:
-            # TODO resolve name using congress-legislators data, using the 
-            # last name, state, and chamber for disambiguation where needed
+        # Resolve Bioguide ID against congress-legislators data
+        if not bioguide_id:
+            print "Bioguide ID not found in page, resolving"
+            bioguide_id = resolve(legislators, member_link.text)
+            
+            if not bioguide_id:
+                print "Bioguide ID not resolved"
+                todo_resolve.append(member_link)
 
-        if bio_id:
-            print "Bio ID:", bio_id
+        # Download image
+        if bioguide_id:
+            print "Bioguide ID:", bioguide_id
             soup = BeautifulSoup(response.read())
             image_tags = soup.findAll('img')
             
-            for image in image_tags: # TODO: ok for now as only one image on the page
-                filename = os.path.join(outdir, bio_id + ".jpg") # TODO: correct to assume jpg?
+            # TODO: Fine for now as only one image on the page
+            for image in image_tags:
+                 # TODO: Correct to assume jpg?
+                filename = os.path.join(outdir, bioguide_id + ".jpg")
                 if os.path.isfile(filename):
                     print "Image already exists:", filename
                 elif not args.test:
@@ -175,23 +255,29 @@ def download_photos(br, member_links, outdir):
                     save.write(data)
                     save.close()
                 break
-        else:
-            print "Bio ID not found"
-            todo_resolve.append(member_link)
+            
+            # Remove this from our YAML list to prevent any bad resolutions later
+            legislators = remove_from_yaml(legislators, bioguide_id)
 
-    # TODO For each entry here, need to add a resolving case above
-    print "Didn't have bio IDs:", len(todo_resolve)
+    # TODO: For each entry remaining here, check if they've since left Congress.
+    # If not, either need to add a resolving case above, or fix the GPO/YAML data.
+    print "---"
+    print "Didn't resolve Bioguide IDs:", len(todo_resolve)
     for member_link in todo_resolve:
         print member_link.text, member_link.url
 
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Scrape http://www.memberguide.gpoaccess.gov and save members' photos named after their Bio IDs", 
+        description="Scrape http://www.memberguide.gpoaccess.gov and save members' photos named after their Bioguide IDs", 
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-c', '--congress', default='113',
         help="Congress session number, for example: 110, 111, 112, 113")
     parser.add_argument('-o', '--outdir', default="images",
         help="Directory to save photos in")
+    parser.add_argument('--yaml', default='legislators-current.yaml',
+        help="Path to the downloaded https://raw2.github.com/unitedstates/congress-legislators/master/legislators-current.yaml")
     parser.add_argument('-t', '--test', action='store_true',
         help="Test mode: don't actually save images")
     args = parser.parse_args()
