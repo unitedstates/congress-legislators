unitedstates/congress-legislators

commit 31f5c2e685fe9414404b55d8a06008d9b4ba370b

Patch up comment block preservation to not depend on the original object getting passed from load to save, by reading comment block at write-time. Also removes the need for another YAML serializer.

scripts/rtyaml.py
@@ -105,46 +105,35 @@ Dumper.add_representer(unicode, our_string_representer)
 Dumper.add_representer(type(None), lambda dumper, value : \
 	dumper.represent_scalar(u'tag:yaml.org,2002:null', u"~"))
 
-# Use a subclss of list when trying to hold onto a block comment at the
-# start of a stream. Make sure it serializes back to a plain YAML list.
-class RtYamlList(list):
-    pass
-def RtYamlList_serializer(self, data):
-    return self.represent_sequence('tag:yaml.org,2002:seq', data)
-Dumper.add_representer(RtYamlList, RtYamlList_serializer)
 
 # Provide some wrapper methods that apply typical settings.
 
 def load(stream):
-    # Read any comment block at the start. We can only do this if we can
-    # peek the stream. Convert a file to an io.BufferedReader for convenience.
-    # Attempt to read for a comment block if the stream has a peek method.
+    return yaml.load(stream, Loader=Loader)
+
+
+# Read any comment block at the start. We can only do this if we can
+# peek the stream. Convert a file to an io.BufferedReader for convenience.
+# Attempt to read for a comment block if the stream has a peek method.
+def comment_header_from(file):
     initial_comment_block = ""
-    if isinstance(stream, file):
-        stream = io.open(stream.fileno(), mode="rb", closefd=False)
-    if hasattr(stream, "peek") and hasattr(stream, "readline"):
-        while stream.peek(1)[0] == "#":
-            initial_comment_block += stream.readline()
 
-    # Read the object from the stream.
-    obj = yaml.load(stream, Loader=Loader)
+    read_stream = io.open(file.fileno(), mode="rb", closefd=False)
 
-    # Attach our initial comment to the object so we can save it later (assuming
-    # this object is written back out).
-    if initial_comment_block:
-        if isinstance(obj, list):
-            # The list class can't be assigned any custom attributes, but we can make a subclass that can.
-            # Clone the list object into a RtYamlList instance.
-            obj = RtYamlList(obj)
-        obj.__initial_comment_block = initial_comment_block
+    if hasattr(read_stream, "peek") and hasattr(read_stream, "readline"):
+        while read_stream.peek(1)[0] == "#":
+            initial_comment_block += read_stream.readline()
 
-    return obj
+    return initial_comment_block
 
 def dump(data, stream):
-    # If we pulled in an initial comment block when reading the stream, write
-    # it back out at the start of the stream.
-    if hasattr(data, '__initial_comment_block'):
-        stream.write(data.__initial_comment_block)
+    # if we got a file, check if there's an initial comment bloc
+    if isinstance(stream, file):
+        initial_comment_block = comment_header_from(stream)
+        stream.seek(0)
+
+        if initial_comment_block:
+            stream.write(initial_comment_block)
 
     # Write the object to the stream.
     yaml.dump(data, stream, default_flow_style=False, allow_unicode=True, Dumper=Dumper)

scripts/utils.py
@@ -328,7 +328,8 @@ def yaml_load(path, use_cache=True):
     return data
 
 def yaml_dump(data, path):
-    rtyaml.dump(data, open(path, "w"))
+    # open for reading as well as writing
+    rtyaml.dump(data, open(path, "r+"))
 
     # Store in a pickled file for fast access later.
     import cPickle as pickle, hashlib

commit e81a592b6108c22612062c0f20da9ec492de7ab8

fix rtyaml for files that decrease in size
bug caused by 31f5c2e685fe9414404b55d8a06008d9b4ba370b by opening a file with r+ without truncating its contents

scripts/rtyaml.py
@@ -131,6 +131,7 @@ def dump(data, stream):
     if isinstance(stream, file):
         initial_comment_block = comment_header_from(stream)
         stream.seek(0)
+        stream.truncate() # in case file shrinks, make sure we don't leave anything at the end
 
         if initial_comment_block:
             stream.write(initial_comment_block)