938c57b502e4d484a6c616066772ed52008a7930
Joshua Tauberer <jt@occams.info>
2015-01-03 11:13:10 -0500

    three new terms were missing
    Due to special elections, terms for the 114th Congress didn't get added for two reps.
    
    Gregorio Sabalan also didn't get a new term. According to Wikipedia he was elected as an Independent, so I am changing his party. But I assume he will continue to caucus with the Democrats, so I added a caucus field.

--- scripts/validator.py
+++ scripts/validator.py
@@ -7,9 +7,17 @@ P = rtyaml.load(open("../legislators-current.yaml"))
 senate_ranks = { }
 
 for p in P:
+	# Get the current term.
 	term = p['terms'][-1]
 
-	# Collect all of the senate state ranks.
+	if term['start'] not in ('2011-01-05', '2013-01-03', '2015-01-06'):
+		print("Term start date of %s is not right: %s." % (p['id']['bioguide'], term['start']))
+
+	if term['end'] not in ('2017-01-03', '2019-01-03', '2021-01-03'):
+		print("Term end date of %s is not right: %s." % (p['id']['bioguide'], term['end']))
+
+	# Collect all of the senate state ranks so we can check that the distribution
+	# within each state is correct, at the end.
 	if term['type'] == 'sen':
 		senate_ranks.setdefault(term['state'], []).append((p['id']['bioguide'], term['state_rank']))
 
