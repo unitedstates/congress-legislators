ba3e5d1c656a3c24d30e234a6af900c968c34620
Joshua Tauberer <jt@occams.info>
2015-01-06 17:54:32 -0500

    MT is at-large so Zinke's district should have been 0 not 1
    Add a test for this in the new validator.py.

--- scripts/validator.py
+++ scripts/validator.py
@@ -2,6 +2,11 @@
 
 import rtyaml
 
+# Congressional district apportionment for the 113th-... Congresses.
+# The territories with delegates have 'T'. All others have the number
+# of districts (e.g. 1 for one at-large district).
+apportionment = {'AL': 7, 'AK': 1, 'AS': 'T', 'AZ': 9, 'AR': 4, 'CA': 53, 'CO': 7, 'CT': 5, 'DE': 1, 'DC': 'T', 'FL': 27, 'GA': 14, 'GU': 'T', 'HI': 2, 'ID': 2, 'IL': 18, 'IN': 9, 'IA': 4, 'KS': 4, 'KY': 6, 'LA': 6, 'ME': 2, 'MD': 8, 'MA': 9, 'MI': 14, 'MN': 8, 'MS': 4, 'MO': 8, 'MT': 1, 'NE': 3, 'NV': 4, 'NH': 2, 'NJ': 12, 'NM': 3, 'NY': 27, 'NC': 13, 'ND': 1, 'MP': 'T', 'OH': 16, 'OK': 5, 'OR': 5, 'PA': 18, 'PR': 'T', 'RI': 2, 'SC': 7, 'SD': 1, 'TN': 9, 'TX': 36, 'UT': 4, 'VT': 1, 'VI': 'T', 'VA': 11, 'WA': 10, 'WV': 3, 'WI': 8, 'WY': 1}
+
 def run():
 
 	P = rtyaml.load(open("../legislators-current.yaml"))
@@ -13,11 +18,33 @@ def run():
 		# Get the current term.
 		term = p['terms'][-1]
 
+		# Start/end dates.
+
 		if term['start'] not in ('2011-01-05', '2013-01-03', '2015-01-06'):
 			print("Term start date of %s is not right: %s." % (p['id']['bioguide'], term['start']))
 
 		if term['end'] not in ('2017-01-03', '2019-01-03', '2021-01-03'):
 			print("Term end date of %s is not right: %s." % (p['id']['bioguide'], term['end']))
+
+		# State and district.
+
+		if term['state'] not in apportionment:
+			print("Term state in %s is invalid: %s." % (p['id']['bioguide'], term['state']))
+
+		else:
+			if term['type'] == 'rep':
+				ap = apportionment[term['state']]
+				if not isinstance(term['district'], int) or term['district'] < 0:
+					print("Term district in %s is invalid: %s." % (p['id']['bioguide'], str(term['district'])))
+				elif ap in ("T", 1) and term['district'] != 0:
+					print("Term district in %s is invalid for an at-large state: %s." % (p['id']['bioguide'], str(term['district'])))
+				elif ap not in ("T", 1) and term['district'] == 0:
+					print("Term district in %s is invalid for a not-at-large state: %s." % (p['id']['bioguide'], str(term['district'])))
+				elif ap not in ("T", 1) and term['district'] > ap:
+					print("Term district in %s is invalid: %s." % (p['id']['bioguide'], str(term['district'])))
+
+
+		# Seate state rank.
 
 		# Collect all of the senate state ranks so we can check that the distribution
 		# within each state is correct, at the end.
