b195a59fd75fbf81944c5f293f7d7c570eb1f19c
Joshua Tauberer <jt@occams.info>
2015-02-06 18:33:26 -0500

    fill in additional house_history IDs
    Adjusted the scraper to be faster and played with the ID ranges
    to slurp in more people. However I've now noticed that the House
    History website is assigning IDs like 15032386870, which will make
    it take too long (ha!) to scrape this way going forward.

--- scripts/house_history.py
+++ scripts/house_history.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python
 
-# gets bioguide id for every member with a house history ID:
-
-# options:
-#  --cache: load from cache if present on disk (default: true)
-#  --current: do *only* current legislators (default: true)
-#  --historical: do *only* historical legislators (default: false)
-#  --bioguide: do *only* a single legislator
+# Stores a house_history ID for all legislators that don't yet
+# have one, by scraping history.house.gov.
 
 import lxml.html, io
 import utils
@@ -14,48 +9,56 @@ import requests
 from utils import load_data, save_data
 
 def run():
-
-  # pick either current or historical
-  # order is important here, since current defaults to true
-  if utils.flags().get('historical', False):
-    filename = "legislators-historical.yaml"
-  elif utils.flags().get('current', True):
-    filename = "legislators-current.yaml"
-  else:
-    print("No legislators selected.")
-    exit(0)
-
-  print("Loading %s..." % filename)
-  legislators = load_data(filename)
+  # load legislators YAML files
+  yamlfiles = { }
+  for fn in ('historical', 'current'):
+    fn = 'legislators-%s.yaml' % fn
+    print("Loading %s..." % fn)
+    yamlfiles[fn] = load_data(fn)
 
   # reoriented cache to access by bioguide ID
   by_bioguide = { }
-  for m in legislators:
-    if "bioguide" in m["id"]:
-      by_bioguide[m["id"]["bioguide"]] = m
-
+  known_house_history_ids = set()
+  for legislators in yamlfiles.values():
+    for m in legislators:
+      if "bioguide" in m["id"]:
+        by_bioguide[m["id"]["bioguide"]] = m
+      if "house_history" in m["id"]:
+        known_house_history_ids.add(m["id"]["house_history"])
   count = 0
 
-  for id in range(8245,21131):
+  # scrape history.house.gov
+  for id in range(22000, 25000):
+    # skip known IDs
+    if id in known_house_history_ids:
+      continue
     print(id)
+    bioguide_id = get_bioguide_for_house_history_id(id)
+    if bioguide_id and bioguide_id in by_bioguide:
+      print(id, bioguide_id)
+      by_bioguide[bioguide_id]["id"]["house_history"] = id
+      count = count + 1
+
+  # write YAML files to disk
+  for filename, legislators in yamlfiles.items():
+    print("Saving data to %s..." % filename)
+    save_data(legislators, filename)
+
+  # how many updates did we make?
+  print("Saved %d legislators" % count)
+
+def get_bioguide_for_house_history_id(id):
     url = "http://history.house.gov/People/Detail/%s" % id
     r = requests.get(url, allow_redirects=False)
     if r.status_code == 200:
         dom = lxml.html.parse(io.StringIO(r.text)).getroot()
         try:
             bioguide_link = dom.cssselect("a.view-in-bioguide")[0].get('href')
-            bioguide_id = bioguide_link.split('=')[1]
-            by_bioguide[bioguide_id]["id"]["house_history"] = id
-            count = count + 1
+            return bioguide_link.split('=')[1]
         except:
-            continue
+            return None
     else:
-        continue
-
-  print("Saving data to %s..." % filename)
-  save_data(legislators, filename)
-
-  print("Saved %d legislators to %s" % (count, filename))
+        return None
 
 if __name__ == '__main__':
   run()
