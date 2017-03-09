45a0ab55599fb574dbacff15535113c2b089905d
Joel Collins <jconline@gmail.com>
2017-03-08 23:11:43 -0500

    (FORCE) new commit with test data removed, generate json on saving data

---
+++ scripts/generate_json.py
@@ -0,0 +1,21 @@
+import json
+import utils
+import glob
+import os
+
+def run():
+
+	#yaml filenames
+    yamls = list(map(os.path.basename, glob.glob("../*.yaml")))
+
+    for filename in yamls:
+        print("Converting %s..." % filename)
+        data = utils.load_data(filename)
+
+		#convert yaml to json
+        utils.write(
+            json.dumps(data, default=utils.format_datetime),
+            "../alternate_formats/%s.json" %filename.replace(".yaml", ""))
+
+if __name__ == '__main__':
+    run()
